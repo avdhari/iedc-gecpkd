d18f2ec45a19f7dc4de42f12d305e6094adc9c88
Aravind Hari <avdhari114@gmail.com>
2021-09-05 11:56:22 +0530

    changed DB to psql

--- iedc/settings.py
+++ iedc/settings.py
@@ -79,11 +79,21 @@ WSGI_APPLICATION = 'iedc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'iedc',
+
+        'USER': 'avdhari',
+
+        'PASSWORD': 'askme',
+
     }
+
 }
 
 
