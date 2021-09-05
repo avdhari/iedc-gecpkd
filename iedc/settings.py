09d5094193e8712c66ecb33d384a1c265324c3ce
Aravind Hari <avdhari114@gmail.com>
2021-09-05 12:53:29 +0530

    added DB connection

--- iedc/settings.py
+++ iedc/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,12 +95,13 @@ DATABASES = {
         'PASSWORD': 'askme',
 
         'HOST': 'localhost',
-
-
-
     }
 
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
