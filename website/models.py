4d2c14710cbb232a54be713fc824a755f10499d6
Aravind Hari <avdhari114@gmail.com>
2021-09-12 12:08:40 +0530

    changed ImageField to URLField and related config in templates

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     register = models.CharField(max_length=200)
-    flyer = models.ImageField(blank=False)
+    flyer = models.URLField(default="https://drive.google.com/uc?id=")
     date_created = models.DateTimeField(auto_now_add=True)
     event_date = models.DateTimeField(auto_now=False)
     event_is_live = models.BooleanField()
