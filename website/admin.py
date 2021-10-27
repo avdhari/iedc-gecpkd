d3c8b0a63739ea38c525509b52e126578e657425
Aravind Hari <avdhari114@gmail.com>
2021-10-27 22:02:43 +0530

    added about page models

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from website.models import Event, HustleChat
+from website.models import *
 
 
 class DisplayEvent(admin.ModelAdmin):
     list_display = ['title', 'event_date', 'date_created']
 
+class DisplayTeam(admin.ModelAdmin):
+    list_display = ['name', 'position']
 
 admin.site.register(Event, DisplayEvent)
 admin.site.register(HustleChat)
+admin.site.register(NodalOfficer)
+admin.site.register(TeamMember, DisplayTeam)
--- website/models.py
+++ website/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-from django.db.models.base import Model
+from django.db.models.aggregates import Max
 from django.utils.text import slugify
 from embed_video.fields import EmbedVideoField
+from website.tasks import image_url_split
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+        app_label = 'website'
 
 
-class Event(models.Model):
+class Event(BaseModel):
     is_removed = models.BooleanField(default=False)
     title = models.CharField(max_length=150)
     content = models.TextField()
@@ -26,10 +32,52 @@ class Event(models.Model):
         super(Event, self).save(*args, **kwargs)
 
 
-class HustleChat(models.Model):
+class HustleChat(BaseModel):
     name_of_guest = models.CharField(blank=True, max_length=250)
     episode = models.IntegerField()
     video = EmbedVideoField()
 
     def __str__(self):
         return "Episode " + str(self.episode) + ": " + self.name_of_guest
+
+
+
+class NodalOfficer(BaseModel):
+    name = models.CharField(max_length=200)
+    profile_image = models.URLField()
+    linkedin_url = models.CharField(blank=True, max_length=250)
+    twitter_url = models.CharField(blank=True, max_length=250)
+    instagram_url = models.CharField(blank=True, max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        self.profile_image = image_url_split(self.profile_image)
+        super(NodalOfficer, self).save(*args, **kwargs)
+
+
+class TeamMember(BaseModel):
+    name = models.CharField(max_length=200)
+    profile_image = models.URLField()
+    linkedin_url = models.CharField(blank=True, max_length=250)
+    twitter_url = models.CharField(blank=True, max_length=250)
+    instagram_url = models.CharField(blank=True, max_length=250)
+    POSITION = [
+        ('Student Lead', 'Student Lead'),
+        ('Technology Lead', 'Technology Lead'),
+        ('Quality and Operations Lead', 'Quality and Operations Lead'),
+        ('Community Lead', ' Community Lead'),
+        ('Creative and Innovations Lead', 'Creative and Innovations Lead'),
+        ('Finance Lead', 'Finance Lead'),
+        ('Branding and Marketing Lead', 'Branding and Marketing Lead'),
+    ]
+    position = models.CharField(max_length=50, choices=POSITION)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.profile_image = image_url_split(self.profile_image)
+        super(TeamMember, self).save(*args, **kwargs)
